f29d46b4e9310af75c9c64b38df5e5f7f2c0fcec
jonot <48457507+jonot-cyber@users.noreply.github.com>
2022-11-08 16:49:40 -0500

    Revert "Made web interface"

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"log"
 	"math/rand"
-	"net/http"
-	"os"
 	"strings"
 	"time"
 )
@@ -37,7 +34,7 @@ func (m Move) String() string {
 	return b.String()
 }
 
-var sides = []rune{'F', 'B', 'L', 'R', 'U', 'D'}
+var sides []rune = []rune{'F', 'B', 'L', 'R', 'U', 'D'}
 
 func SideAxis(s rune) (int, error) {
 	switch s {
@@ -49,6 +46,10 @@ func SideAxis(s rune) (int, error) {
 		return Z, nil
 	}
 	return 0, errors.New("invalid rune")
+}
+
+func RandomBool() bool {
+	return rand.Intn(2) == 1
 }
 
 func RandomMove(lastMove Move) (Move, error) {
@@ -94,47 +95,15 @@ func RandomMoves(count int) ([]Move, error) {
 }
 
 func main() {
-	gin.SetMode(gin.ReleaseMode)
+	defer func(start time.Time) {
+		fmt.Println(time.Since(start))
+	}(time.Now())
 	rand.Seed(time.Now().UnixMicro())
-	r := gin.Default()
-	r.LoadHTMLFiles("index.gohtml")
-	r.GET("/scramble", func(c *gin.Context) {
+	for i := 0; i < 1_000_000; i++ {
 		moves, err := RandomMoves(20)
 		if err != nil {
-			c.Status(http.StatusInternalServerError)
-			return
+			log.Fatal(err)
 		}
-		res := make([]string, len(moves))
-		for i := range res {
-			res[i] = moves[i].String()
-		}
-		c.JSON(http.StatusOK, res)
-	})
-	r.GET("/", func(c *gin.Context) {
-		moves, err := RandomMoves(20)
-		if err != nil {
-			c.Status(http.StatusInternalServerError)
-			return
-		}
-		res := make([]string, len(moves))
-		for i := range res {
-			res[i] = moves[i].String()
-		}
-		c.HTML(http.StatusOK, "index.gohtml", gin.H{
-			"scramble": strings.Join(res, " "),
-		})
-	})
-	port := "8080"
-	if len(os.Args) > 2 {
-		fmt.Println("USAGE: ./cubiks <port>")
-		fmt.Println("       ./cubiks (defaults to 8080)")
-		return
-	}
-	if len(os.Args) == 2 {
-		port = os.Args[1]
-	}
-	err := r.Run(":" + port)
-	if err != nil {
-		log.Fatal(err)
+		fmt.Println(moves)
 	}
 }
