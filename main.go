b1d9574d000b7f67cd19e46ba803282fd5ea5397
Joseph Tessitore <joetgamerman3@gmail.com>
2022-11-08 15:08:13 -0500

    Fixed issue #1

--- main.go
+++ main.go
@@ -1,44 +1,109 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"math/rand"
-	"os"
-	"strconv"
 	"strings"
 	"time"
 )
 
-var sides []rune = []rune{'F', 'B', 'L', 'R', 'U', 'D'}
+const (
+	X = iota
+	Y = iota
+	Z = iota
+)
 
-func RandomBool() bool {
-	return rand.Intn(2) == 1
+type Move struct {
+	Side  rune
+	Axis  int
+	Count int
 }
 
-func RandomChoice() string {
+func (m Move) String() string {
 	var b strings.Builder
-	side := sides[rand.Intn(len(sides))]
-	b.WriteRune(side)
-	if RandomBool() {
+	b.WriteRune(m.Side)
+	switch m.Count {
+	case 1: // do nothing
+	case 2:
+		b.WriteRune('2')
+	case 3:
 		b.WriteRune('\'')
 	}
 	return b.String()
 }
 
-func main() {
-	rand.Seed(time.Now().UnixMicro())
+var sides []rune = []rune{'F', 'B', 'L', 'R', 'U', 'D'}
 
-	times := 20
-	if len(os.Args) >= 2 {
-		arg, err := strconv.Atoi(os.Args[1])
-		if err != nil {
-			log.Fatal("Argument must be a number")
+func SideAxis(s rune) (int, error) {
+	switch s {
+	case 'L', 'R':
+		return X, nil
+	case 'U', 'D':
+		return Y, nil
+	case 'F', 'B':
+		return Z, nil
+	}
+	return 0, errors.New("invalid rune")
+}
+
+func RandomBool() bool {
+	return rand.Intn(2) == 1
+}
+
+func RandomMove(lastMove Move) (Move, error) {
+	var availableSides []rune
+	defaultMove := Move{}
+	if lastMove == defaultMove {
+		availableSides = sides
+	} else {
+		for _, v := range sides {
+			x, err := SideAxis(v)
+			if err != nil {
+				return Move{}, err
+			}
+			if x != lastMove.Axis {
+				availableSides = append(availableSides, v)
+			}
 		}
-		times = arg
 	}
-	for i := 0; i < times; i++ {
-		fmt.Print(RandomChoice(), " ")
+	side := availableSides[rand.Intn(len(availableSides))]
+	axis, err := SideAxis(side)
+	if err != nil {
+		return Move{}, err
 	}
-	fmt.Println()
+	return Move{
+		Side:  side,
+		Axis:  axis,
+		Count: rand.Intn(3) + 1,
+	}, nil
+}
+
+func RandomMoves(count int) ([]Move, error) {
+	res := make([]Move, count)
+	lastMove := Move{}
+	for i := 0; i < count; i++ {
+		move, err := RandomMove(lastMove)
+		lastMove = move
+		if err != nil {
+			return nil, err
+		}
+		res[i] = move
+	}
+	return res, nil
+}
+
+func main() {
+	defer func(start time.Time) {
+		fmt.Println(time.Since(start))
+	}(time.Now())
+	rand.Seed(time.Now().UnixMicro())
+	for i := 0; i < 1_000_000; i++ {
+		moves, err := RandomMoves(20)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(moves)
+	}
 }
